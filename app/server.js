4e82e0fd8ef05691c76ca07e7a39ea69559f0dab
8pockets <8pockets@gmail.com>
2016-05-08 19:57:02 +0900

    DB接続完了

--- app/server.js
+++ app/server.js
@@ -1,31 +1,63 @@
 'use strict';
 
 const express = require('express');
-const mongoose = require('mongoose');
+const mysql = require('mysql');
+const winston = require('winston');
 
 const app = express();
 
 app.set('port', 5000);
 app.use(express.static(__dirname + '/public'));
 
+//winston setting
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+      level: 'silly', // level は silly 以上
+      colorize: true, // 色付き
+      timestamp: true // 時間つき
+    }),
+      new (winston.transports.File)({
+      filename: '/var/log/node_app/node_app.log',
+      json: false
+    })
+    ]
+});
+
 app.get('/', function(request, response) {
-/*
-  mongoose.connect('mongodb://localhost:27017/foo');
 
-  var Cat = mongoose.model('Cat', { name: String });
+  logger.silly('ばかばかしいこと');
+  logger.debug('とてもどうでもいいこと');
+  logger.verbose('どうでもいいこと');
+  logger.info('じょうほう');
+  logger.warn('やばい');
+  logger.error('すごくやばい');
 
-  var kitty = new Cat({ name: 'Zildjian' });
-  kitty.save(function (err) {
-    if (err) {
-      console.log(err);
+  response.send('Hello World!');
+});
+
+app.get('/mysql', function(request, response) {
+
+  var connection = mysql.createConnection({
+    host     : 'mysql',
+    port     : '3306',
+    user     : 'root',
+    password : 'root',
+    database : 'node_app'
+  });
+
+  connection.connect(function(err){
+    if(!err) {
+        logger.info("Database is connected ...");
     } else {
-      console.log('meow');
+        logger.error(err);
+        return;
     }
   });
-*/
-  response.send('Hello World!')
+
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
+
